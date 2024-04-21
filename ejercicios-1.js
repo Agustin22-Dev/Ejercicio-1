7aaadf607e5c140ad0aba730603909f8cca8db3b
Keneolik <keneolik22@gmail.com>
2024-04-21 20:07:19 -0300

    Ejercicios 1.0

---
+++ ejercicios-1.js
@@ -0,0 +1,106 @@
+//ejercicio 1
+
+//const valor1=22
+//const valor2=8
+//suma= valor1+valor2
+//console.log (suma)
+
+//ejercicio 2
+// const x= 10
+// const y=7
+// const z=12
+
+// if (x >y && x > z) {
+//      may=x
+// }
+// if (y>x && y>z) {
+//     may=y
+// }
+// if (z>x && z>y) {
+//     may=z
+// }
+// if (x<y && x<z) {
+//     min=x
+// }
+// if (y<x&&x<z) {
+//     min=y
+// }
+// if (z<x && z<y) {
+//     min=z
+// }
+// console.log (may)
+// console.log (min)    
+
+//ejercicio 3
+
+// var palabra= "pepperoni"
+// console.log("la palabra",palabra,"tiene",palabra.length,"caracteres")
+
+//ejercicio 4
+// let num1= 20
+// if (num1%2 == 0) {
+//     console.log ("su numero es par");
+
+// }
+// else {
+//    console.log ("su numero no es par");
+// }
+
+//ejercicio 5
+// var palabra="pepperoni"
+// var numero= 5
+// var bucle=0
+// while (bucle != numero)
+// {
+//     if
+//         (bucle==numero)
+//         break
+//     console.log(palabra);
+//     bucle++
+// }
+
+//ejercicio 6
+// const numero1=5
+// const numero2=10
+// var cadenanumeros ="6,7,8,9"
+// var coma=","
+// var arraycadena=cadenanumeros.split(coma)
+
+// console.log("entre el numero",numero1,"y el numero",numero2,"hay una distancia de",arraycadena.length,"elementos")
+// console.log("5[6-7-8-9]10")
+
+//ejercicio 7 (prueba)
+// var valor1=prompt("deme el valor 1",0);
+// var valor2=prompt("deme el valor 2",0);
+// while (valor1 < valor2) {
+//     if (valor1%3==0)
+//        alert(valor1)
+//     valor1=valor+1
+// }
+
+//ejercicio 7 
+/* <script>
+        let a=1
+        let b=100
+        while (a<=b) {
+            if(a%3==0){
+                document.write(a+",");
+            }
+            if(a%5==0){
+                document.write(a+","):
+            }
+        a=a+1
+        }
+    </script> */
+
+//ejercicio 8
+// let total=0
+// let numeros= [1,2,3,4,5]
+// for(let i of numeros) total+=i;
+// console.log(total);
+
+//ejercicio 9
+// let total=0
+// let numeros= [1,2,3,4,5]
+// for(let i of numeros) total+=i;
+// console.log(total);
